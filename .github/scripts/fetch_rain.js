64e7262e2712e9e0a2f18851e19515b5ba260e40
moacyrsb <moacyrsb@hotmail.com>
2025-04-23 15:03:16 -0600

    Update fetch_rain.js

--- .github/scripts/fetch_rain.js
+++ .github/scripts/fetch_rain.js
@@ -2,7 +2,10 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+  headless: "new",
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
 
   await page.goto('https://dex.cocorahs.org/stations/SD-DV-38/obs-tables', {
